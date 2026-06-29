e5275cd593ed2772272a7aba1329c74d61c37b3d
Arpit Srivastava <geniusbillionairearpit@gmail.com>
2026-06-29 18:02:54 +0530

    Fix Nodemailer SMTP

--- Backend/config/mail.js
+++ Backend/config/mail.js
@@ -12,6 +12,26 @@ transporter.verify((error) => {
     if (error) {
         console.log("Mail Error", error);
     } else {
+        console.log("Mail Server Ready");
+    }
+});
+
+module.exports = transporter;
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    }
+});
+
+transporter.verify((error) => {
+    if (error) {
+        console.log("Mail Error", error);
+    } else {
         comsole.log("Mail Server Ready");
     }
 });
