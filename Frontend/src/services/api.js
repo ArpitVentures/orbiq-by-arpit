d22256552671964d9aa439dcfa3681a4a2a4ffcf
Arpit Srivastava <geniusbillionairearpit@gmail.com>
2026-08-23 01:20:58 +0530

    Fix production API base URL

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,20 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_SERVER_URL
+    baseURL: import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
 });
 
 api.interceptors.request.use((config) => {
-
     const token = sessionStorage.getItem("token");
-
-    console.log("Interceptor Token:", token);
 
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
-
-    console.log("Headers:", config.headers);
 
     return config;
 });
